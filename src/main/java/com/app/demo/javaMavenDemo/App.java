7536f7ed00f2a2e9da39f48666dc78d82dc72180
VenuMadhav21 <venumadhav721@gmail.com>
2024-11-14 12:19:37 +0530

    Sample Maven

---
+++ src/main/java/com/app/demo/javaMavenDemo/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.javaMavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Venu Madhav - 22BD1A054B" );
+    }
+}
